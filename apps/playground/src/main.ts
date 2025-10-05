454156ebfce2f8b79f08a024d4f8b09b283ff505
Gimwa Chan <ou.kinkin227@gmail.com>
2025-10-05 11:51:23 +0900

    setup

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,11 +1,9 @@
-import { setupCounter } from './counter.ts'
-import '../style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+import counter from "./components/counter.html" with { type: "html" };
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "/vite.svg";
 
-
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -13,15 +11,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
+`;
 
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(counter);
+});
--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,36 +1,6 @@
-import { inputAnalyzer } from "@vanira/vite-plugin-input-analyzer";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
+import { htmlModules } from "@vanira/vite-plugin-html-module";
 import { defineConfig } from "vite";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const main = resolve(__dirname, "pages/index.html");
-const nested = resolve(__dirname, "pages/nested/index.html");
-
-console.log("main", main);
-console.log("nested", nested);
-
 export default defineConfig({
-  plugins: [
-    inputAnalyzer({
-      logToConsole: true,
-      generateReport: true,
-      reportPath: "./build-analysis.json",
-      onAnalyze: (inputConfig, analysis) => {
-        console.log("🎯 custom analysis callback:", {
-          config: inputConfig,
-          stats: `${analysis.totalEntries} entries`,
-          fileType: analysis.fileTypeStats,
-        });
-      },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main,
-        nested,
-      },
-    },
-  },
+  plugins: [htmlModules()],
 });
